4e9b3c2c3f75023d470d6aac95bd41b0f0f6dcab
Mohamed Chalal <Mohamedrchalal@gmail.com>
2016-03-02 16:53:48 -0500

    refactored

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function startGame(){
-  deadpoolTrivia = {
+  var deadpoolTrivia = {
     questions: [
       'What DC comics character is Deadpool similar to?',
       'What is deadpools real name?',
@@ -14,81 +14,87 @@ function startGame(){
       'which country is deadpool from?'
     ],
     answersA: ['a. Superman','a. slade wilson','a. Desert Storm',
-    'a. Wolverine','a. regenration','a. wolverine',
-    'a. childhood nickname','a. heterosexual','a. thomas',
-    'a. great wall of china','a. Mexico'],
+      'a. Wolverine','a. regenration','a. wolverine',
+      'a. childhood nickname','a. heterosexual','a. thomas',
+      'a. great wall of china','a. Mexico'],
     answersB: ['b. batman','b. clark kent','b. CIA',
-    'b. colossus','b. quick wit','b. thor',
-    'b. pet name','b. homosexual','b. muhammad',
-    'b. berlin wall','b. USA'],
+      'b. colossus','b. quick wit','b. thor',
+      'b. pet name','b. homosexual','b. muhammad',
+      'b. berlin wall','b. USA'],
     answersC:['c. wonder woman','c. peter parker','c. S.H.I.E.L.D.',
-    'c. blackwidow','c. fast reflexes','c. hulk',
-    'c. a historical massacre','c. bisexual','c. leroy jenkins',
-    'c. vaginal wall','c. UK'],
+      'c. blackwidow','c. fast reflexes','c. hulk',
+      'c. a historical massacre','c. bisexual','c. leroy jenkins',
+      'c. vaginal wall','c. UK'],
     answersD: ['d. deathstroke','d. wade wilson','d. weapon X',
-    'd. spiderman','d. all of the above','d. captain america',
-    'd. a bet','d. omnisexual','d. francis',
-    'd. fourth wall','d. Canada'],
+      'd. spiderman','d. all of the above','d. captain america',
+      'd. a bet','d. omnisexual','d. francis',
+      'd. fourth wall','d. Canada'],
     correctAnswers: ['d. deathstroke','d. wade wilson','d. weapon X',
-    'd. spiderman','d. all of the above','d. captain america',
-    'd. a bet','d. omnisexual','d. francis',
-    'd. fourth wall','d. Canada'],
+      'd. spiderman','d. all of the above','d. captain america',
+      'd. a bet','d. omnisexual','d. francis',
+      'd. fourth wall','d. Canada'],
   };
-  var score = 0;
-  var totalScore = 0;
-  var time = 0;
-  var i = 0;
-  var questions = document.getElementById('questions');
-  var answerA = document.getElementById('answerA');
-  var answerB = document.getElementById('answerB');
-  var answerC = document.getElementById('answerC');
-  var answerD = document.getElementById('answerD');
-  var TimerStart = document.getElementById('startTimer')
-  document.getElementById('score').innerHTML = totalScore;
-  document.getElementById('time').innerHTML = time;
+
+      var score = 0;
+      var totalScore = 0;
+      var time = 0;
+      var i = 0;
+      var questions = $('#questions');
+      var answerA = $('#answerA');
+      var answerB = $('#answerB');
+      var answerC = $('#answerC');
+      var answerD = $('#answerD');
+      var TimerStart = $('#startTimer')
+      function scoreUpdate(){
+      $('#score').html(totalScore);
+    };
+      scoreUpdate();
+      function timeUpdate(){
+        $('#time').html(time);
+      };
+      timeUpdate();
+
 
   function populateQuestionAnswers(){
-    questions.innerHTML = deadpoolTrivia.questions[i];
-    answerA.innerHTML = deadpoolTrivia.answersA[i];
-    answerB.innerHTML = deadpoolTrivia.answersB[i];
-    answerC.innerHTML = deadpoolTrivia.answersC[i];
-    answerD.innerHTML = deadpoolTrivia.answersD[i];
+    questions.html(deadpoolTrivia.questions[i]);
+    answerA.html(deadpoolTrivia.answersA[i]);
+    answerB.html(deadpoolTrivia.answersB[i]);
+    answerC.html(deadpoolTrivia.answersC[i]);
+    answerD.html(deadpoolTrivia.answersD[i]);
   }
   populateQuestionAnswers()
 
-  startTimer = setInterval(Timer,1000);
+  var startTimer = setInterval(Timer,1000);
 
   function Timer(){
     time++;
-    document.getElementById('time').innerHTML = time;
+    timeUpdate();
   };
 
   nextQuestion = function(){
     if (this.innerHTML == deadpoolTrivia.correctAnswers[i]){
       score++;
-      console.log(score,i)
     }
     totalScore = Math.floor((score*10000000)/time);
-    i=i+1;
+    i++;
     populateQuestionAnswers();
-    document.getElementById('score').innerHTML = totalScore;
-    saveScore()
+    scoreUpdate();
+    // saveScore();
     if (i>=deadpoolTrivia.questions.length){
-      console.log(score,i);
       clearInterval(startTimer)
       alert('your score is'+ (totalScore) +'!')
       startGame();
     }
   }
 
-function saveScore(){
-  var scoreValue = totalScore;
-  localStorage.setItem('text', scoreValue)
-}
+// function saveScore(){
+//   var scoreValue = totalScore;
+//   localStorage.setItem('text', scoreValue)
+// }
 
-  eventListening = function(){
-    for (j=0;j<document.querySelectorAll('.answers').length;j++){
-      document.querySelectorAll('.answers')[j].addEventListener('click',nextQuestion);
+eventListening = function(){
+    for (j=0;j<$('.answers').length;j++){
+      $('.answers').eq(j).on('click',nextQuestion);
     }
   }
 }
